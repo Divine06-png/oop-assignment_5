95dc29d5c150b289b689d512270e047fc2d4c4e8
Divine Phillip <phillipdivine778@gmail.com>
2026-05-25 11:55:10 -0400

    Add files via upload

---
+++ lec8_page6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int adder(int a, int b){
+    return a+b;
+}
+double adder(double a, double b){
+    return(double)(a+b);
+}
+int main(){
+adder(5,10);
+adder(5,10);
+
+return 0;
+}
